5e3927e9c663bfe3588fdc9f1f3a0642124eaa27
Joseph Phillips <joseph@josephphillips.co>
2024-12-30 08:32:25 -0500

    using avalonia for desktop app with page navigation.

--- ComicBin/Program.cs
+++ ComicBin/Program.cs
@@ -1,41 +1,22 @@
-﻿internal class Program
-{
-    [STAThread]
-    static void Main(string[] args)
-    {
+using Avalonia;
+using Avalonia.ReactiveUI;
+using System;
 
-        // Start the WPF application on a dedicated STA thread
-        Thread uiThread = new Thread(() =>
-        {
-            //Initialize the WPF App from UIDesktopApp1
-            var wpfApp = new ComicBin.Desktop.App();
-            wpfApp.InitializeComponent(); // Load App.xaml
-            wpfApp.Run(); // Start the WPF app
-        })
-        {
-            IsBackground = false, // Ensure the UI thread keeps running
-            Name = "WPF_UI_Thread"
-        };
+namespace ComicBin;
 
-        uiThread.SetApartmentState(ApartmentState.STA);
-        uiThread.Start();
+  internal class Program
+  {
+      // Initialization code. Don't use any Avalonia, third-party APIs or any
+      // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+      // yet and stuff might break.
+      [STAThread]
+      public static void Main(string[] args) => BuildAvaloniaApp()
+          .StartWithClassicDesktopLifetime(args);
 
-        // Separate high-priority thread for background tasks
-        Thread highPriorityThread = new Thread(PerformHighPriorityTasks)
-        {
-            IsBackground = true, // Allows the process to exit when the main thread exits
-            Priority = ThreadPriority.Highest
-        };
-
-        highPriorityThread.Start();
-    }
-
-    private static void PerformHighPriorityTasks(object? obj)
-    {
-        while (true)
-        {
-            Console.WriteLine("Performing high-priority tasks...");
-            Thread.Sleep(500); // Simulate work
-        }
-    }
-}
+      // Avalonia configuration, don't remove; also used by visual designer.
+      public static AppBuilder BuildAvaloniaApp()
+          => AppBuilder.Configure<App>()
+              .UsePlatformDetect()
+              .LogToTrace()
+              .UseReactiveUI();
+  }
---
+++ ComicBin/Views/SettingsPageView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ComicBin.Views
+{
+    public partial class SettingsPageView : UserControl
+    {
+        public SettingsPageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
