b6bd24287651e92d1a7634bc7e98d91ce7e2aa75
Joseph Phillips <joseph@josephphillips.co>
2025-01-05 15:37:28 -0500

    add SetupView

---
+++ ComicBin/Views/Pages/SetUpView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ComicBin;
+
+public partial class SetUpView : UserControl
+{
+    public SetUpView()
+    {
+        InitializeComponent();
+    }
+}
